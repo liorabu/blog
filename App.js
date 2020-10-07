afde3e645c3160239b9a468f0dd91aee30bf96a0
lior abutbul <liorab44@gmail.com>
2020-10-07 13:42:46 +0300

    wrap App.js in blog.context

--- App.js
+++ App.js
@@ -1,29 +1,29 @@
 import 'react-native-gesture-handler';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/contexts/BlogContext'
 
+const Stack = createStackNavigator();
 
-
-
-
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+     return <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Index"
+        screenOptions={{ gestureEnabled: false }}>
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={{ title: 'Blogs List' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  )
+}
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default IndexScreen = () => {
+    return (
+        <View>
+            <Text>index Screeen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
